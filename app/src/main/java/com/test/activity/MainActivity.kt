1d12b22e2a102604217e17366071357ca1d949b2
Vladimir Markeev <fc.vladimir@gmail.com>
2020-09-10 22:02:00 +0300

    initial commit

---
+++ app/src/main/java/com/test/activity/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.test.activity
+
+import android.os.Bundle
+import com.test.R
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
